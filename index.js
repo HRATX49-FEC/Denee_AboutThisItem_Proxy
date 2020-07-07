be27af66df20c188f5b5864f870b94d609c8f6a3
Denee Lopez <denee.lopez@gmail.com>
2020-07-07 14:21:23 -0500

    proxy mvp

--- index.js
+++ index.js
@@ -2,30 +2,44 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const httpProxy = require('http-proxy');
+const compression = require('compression');
+
 const app = express();
+
 const PORT = 8100;
+
 var proxy = httpProxy.createProxyServer({});
+
+app.use(compression());
+
+app.all('/main*', (req, res) => {
+  console.log('connected to mainImage service')
+  proxy.web(req, res, {target: 'http://purrgetmainitemdisplay.us-east-2.elasticbeanstalk.com/'});
+});
+
+app.all('/about*', (req, res) => {
+  console.log('connected to about service');
+  proxy.web(req, res, {target: 'http://purrgetaboutthisitem-dev.us-east-1.elasticbeanstalk.com/'});
+});
+
+app.all('/reccomended*', (req, res) => {
+  console.log('connected to reccomended service')
+  proxy.web(req, res, {target: 'http://rec-feat-display.us-east-2.elasticbeanstalk.com/'});
+});
+
+app.all('/search*', (req, res) => {
+  console.log('connected to search service')
+  proxy.web(req, res, {target: `http://v50-dev.us-east-1.elasticbeanstalk.com/`});
+});
+
+app.all('/reviews*', (req, res) => {
+  console.log('connected to reviews service')
+  proxy.web(req, res, {target: 'http://service-dev2.us-west-2.elasticbeanstalk.com/'});
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-app.get('/main*', (req, res) => {
-  console.log('redirecting to mainImage server')
-  proxy.web(req, res, {target: `http://purrgetmainitemdisplay-env.eba-upicdvwk.us-east-2.elasticbeanstalk.com`});
-});
-
-app.get('/about', (req, res) => {
-  console.log('connected to server')
-  proxy.web(req, res, {target: `http://purrgetaboutthisitem-dev.us-east-1.elasticbeanstalk.com`});
-});
-
-app.get('/reccomended*', (req, res) => {
-  console.log('Redirecting to reccomended service')
-  proxy.web(req, res, {target: 'http://reccomended-featuredserver-dev.us-east-2.elasticbeanstalk.com'})
-})
 
 
 app.listen(PORT, () => {
